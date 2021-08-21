b325816763e5ac2db6890b4511f91e8ef14f2a73
semihM <smhmmc98@gmail.com>
2021-08-21 23:59:18 +0300

    Add 'fetch' function

--- exmatstdlib/ExStdNet.cs
+++ exmatstdlib/ExStdNet.cs
@@ -5,7 +5,9 @@ using System.Net;
 using ExMat.API;
 using ExMat.Objects;
 using ExMat.VM;
+using HtmlAgilityPack;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ExMat.BaseLib
 {
@@ -30,13 +32,138 @@ namespace ExMat.BaseLib
             return SearchEngines[func] + "&key=" + ReadAPIKey(func) + "&q=" + query + "&start=1";
         }
 
-        private static dynamic CreateWebRequest(string link)
+        /// <summary>
+        /// Converts HTML to plain text / strips tags.
+        /// </summary>
+        /// <param name="html">The HTML.</param>
+        /// <returns></returns>
+        public static string ConvertToPlainText(string html)
         {
-            WebRequest request = WebRequest.Create(link);
+            HtmlDocument doc = new();
+            doc.LoadHtml(html);
+
+            StringWriter sw = new();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        /// <summary>
+        /// Count the words.
+        /// The content has to be converted to plain text before (using ConvertToPlainText).
+        /// </summary>
+        /// <param name="plainText">The plain text.</param>
+        /// <returns></returns>
+        public static int CountWords(string plainText)
+        {
+            return !string.IsNullOrEmpty(plainText) ? plainText.Split(' ', '\n').Length : 0;
+        }
+
+        public static string Cut(string text, int length)
+        {
+            if (!string.IsNullOrEmpty(text) && text.Length > length)
+            {
+                text = text.Substring(0, length - 4) + " ...";
+            }
+            return text;
+        }
+
+        private static void ConvertContentTo(HtmlNode node, TextWriter outText)
+        {
+            foreach (HtmlNode subnode in node.ChildNodes)
+            {
+                ConvertTo(subnode, outText);
+            }
+        }
+
+        private static void ConvertTo(HtmlNode node, TextWriter outText)
+        {
+            string html;
+            switch (node.NodeType)
+            {
+                case HtmlNodeType.Comment:
+                    // don't output comments
+                    break;
+
+                case HtmlNodeType.Document:
+                    ConvertContentTo(node, outText);
+                    break;
+
+                case HtmlNodeType.Text:
+                    // script and style must not be output
+                    string parentName = node.ParentNode.Name;
+                    if ((parentName == "script") || (parentName == "style"))
+                    {
+                        break;
+                    }
+
+                    // get text
+                    html = ((HtmlTextNode)node).Text;
+
+                    // is it in fact a special closing node output as text?
+                    if (HtmlNode.IsOverlappedClosingElement(html))
+                    {
+                        break;
+                    }
+
+                    // check the text is meaningful and not a bunch of whitespaces
+                    if (html.Trim().Length > 0)
+                    {
+                        outText.Write(HtmlEntity.DeEntitize(html) + ' ');
+                    }
+                    break;
+
+                case HtmlNodeType.Element:
+                    switch (node.Name)
+                    {
+                        case "p":
+                            // treat paragraphs as crlf
+                            outText.Write("\r\n");
+                            break;
+                        case "br":
+                            outText.Write("\r\n");
+                            break;
+                    }
+
+                    if (node.HasChildNodes)
+                    {
+                        ConvertContentTo(node, outText);
+                    }
+                    break;
+            }
+        }
+
+        private static object CreateWebRequest(string link)
+        {
+            WebRequest request = WebRequest.Create(new UriBuilder(link).Uri);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             using StreamReader str = new(response.GetResponseStream());
             string responseText = str.ReadToEnd();
-            return JsonConvert.DeserializeObject(responseText);
+
+            int idx = response.ContentType.IndexOf(';');
+            string type = idx == -1 ? response.ContentType : response.ContentType.Substring(0, idx);
+
+            switch (type)
+            {
+                case "application/json":
+                    {
+                        return JsonConvert.DeserializeObject(responseText);
+                    }
+                case "text/html":
+                    {
+                        HtmlDocument doc = new();
+                        doc.LoadHtml(responseText);
+
+                        StringWriter sw = new();
+                        ConvertTo(doc.DocumentNode, sw);
+                        sw.Flush();
+                        return sw.ToString();
+                    }
+                default:
+                    {
+                        return null;
+                    }
+            }
         }
 
         public static ExFunctionStatus Wiki(ExVM vm, int nargs)
@@ -90,12 +217,84 @@ namespace ExMat.BaseLib
             }
         }
 
+        private static ExObject GetJsonContent(JToken item)
+        {
+            if (item is JValue)
+            {
+                return new(item.ToString());
+            }
+            else if (item is JProperty i)
+            {
+                return new(i.Value.ToString());
+            }
+            else if (item is JArray ja)
+            {
+                List<ExObject> res = new();
+                foreach (JToken tkn in ja)
+                {
+                    res.Add(GetJsonContent(tkn));
+                }
+                return new(res);
+            }
+            else if (item is JObject)
+            {
+                Dictionary<string, ExObject> res = new();
+                foreach (JProperty tkn in item.Children())
+                {
+                    res.Add(tkn.Name, GetJsonContent(tkn));
+                }
+                return new(res);
+            }
+            return new();
+        }
+
+        public static ExFunctionStatus Fetch(ExVM vm, int nargs)
+        {
+            string link = vm.GetArgument(1).GetString().Trim();
+
+            if (string.IsNullOrWhiteSpace(link))
+            {
+                return vm.CleanReturn(nargs + 2, new ExObject());
+            }
+
+            try
+            {
+                dynamic deserializedResponse = CreateWebRequest(link);
+
+                if (deserializedResponse is string res)
+                {
+                    return vm.CleanReturn(nargs + 2, res);
+                }
+                else if (deserializedResponse is JObject obj && obj.Count > 0)
+                {
+                    Dictionary<string, ExObject> Results = new();
+                    foreach (JProperty item in obj.Children())
+                    {
+                        Results.Add(item.Name, GetJsonContent(item.Value));
+                    }
+                    return vm.CleanReturn(nargs + 2, Results);
+                }
+                return vm.CleanReturn(nargs + 2, new ExObject());
+            }
+            catch (Exception e)
+            {
+                return vm.AddToErrorMessage("FETCH ERROR: " + e.Message);
+            }
+        }
         private static readonly List<ExRegFunc> _stdnetfuncs = new()
         {
             new()
             {
                 Name = "wiki",
                 Function = Wiki,
+                nParameterChecks = 2,
+                ParameterMask = ".s"
+            },
+
+            new()
+            {
+                Name = "fetch",
+                Function = Fetch,
                 nParameterChecks = 2,
                 ParameterMask = ".s"
             }
