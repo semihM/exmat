9eda3ce23e3904f586deebd519f646e1e56be168
semihM <smhmmc98@gmail.com>
2021-08-29 15:15:48 +0300

    Add more utility methods

--- exmat/ExUtils.cs
+++ exmat/ExUtils.cs
@@ -1,10 +1,65 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ExMat.Objects;
 
 namespace ExMat.Utils
 {
     public static class ExUtils
     {
+
+        public static int LongTo32NonNegativeIntegerRange(long i)
+        {
+            if (i < 0)
+            {
+                i = i > int.MinValue ? System.Math.Abs(i) : 0;
+            }
+
+            return i > int.MaxValue ? int.MaxValue : (int)i;
+        }
+
+        public static int LongTo32SignedIntegerRange(long i)
+        {
+            if (i < 0)
+            {
+                return i >= int.MinValue ? (int)i : int.MinValue;
+            }
+            else
+            {
+                return i > int.MaxValue ? int.MaxValue : (int)i;
+            }
+        }
+
+        public static List<ExObject> ShuffleList(List<ExObject> list)
+        {
+            System.Random rand = new();
+            List<ExObject> res = GetACopyOf(list).OrderBy(a => rand.Next()).ToList();
+            return res;
+        }
+
+        public static List<ExObject> GetACopyOf(List<ExObject> list)
+        {
+            List<ExObject> res = new(list.Count);
+            for (int i = 0; i < list.Count; i++)
+            {
+                res.Add(new(list[i]));
+            }
+            return res;
+        }
+
+        public static List<ExObject> GetNRandomObjectsFrom(List<ExObject> list, int n)
+        {
+            System.Random rand = new();
+            List<ExObject> res = new(n);
+            List<ExObject> copy = GetACopyOf(list);
+            while (n > 0)
+            {
+                int idx = rand.Next(n--);
+                res.Add(new(copy[idx]));
+                copy.RemoveAt(idx);
+            }
+            return res;
+        }
+
         public static void InitList<T>(ref List<T> lis, int n) where T : class, new()
         {
             lis = new(n);
