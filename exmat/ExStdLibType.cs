521f2cad5cdb3c66df50ab30b78548787ca9888e
semihM <smhmmc98@gmail.com>
2021-09-08 01:02:57 +0300

    Add attribute ExStdLib and enum ExSydLibType

---
+++ exmat/ExStdLibType.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExMat
+{
+    /// <summary>
+    /// Types for standard libraries
+    /// </summary>
+    public enum ExStdLibType
+    {
+        /// <summary>
+        /// Base library
+        /// </summary>
+        BASE,
+        /// <summary>
+        /// Math library
+        /// </summary>
+        MATH,
+        /// <summary>
+        /// Input-output library
+        /// </summary>
+        IO,
+        /// <summary>
+        /// String library
+        /// </summary>
+        STRING,
+        /// <summary>
+        /// Networking library
+        /// </summary>
+        NETWORK,
+        /// <summary>
+        /// System library
+        /// </summary>
+        SYSTEM
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class ExStdLib : Attribute
+    {
+        public ExStdLibType Type;
+
+        public ExStdLib(ExStdLibType type)
+        {
+            Type = type;
+        }
+    }
+}
