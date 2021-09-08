90e472546cdf9f47bf68c41ebac902e3cb7df97e
semihM <smhmmc98@gmail.com>
2021-09-08 19:56:27 +0300

    Add 'ExStdLibRegister' attribute

---
+++ exmat/ExStdLibRegister.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExMat
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class ExStdLibRegister : Attribute
+    {
+        public string RegisterMethodName;
+
+        public ExStdLibRegister(string name)
+        {
+            RegisterMethodName = name;
+        }
+    }
+}
