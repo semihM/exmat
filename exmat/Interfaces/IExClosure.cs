9adf52b5a42c933ba4ec3aaeb6a55fc662fc9d9c
semihM <smhmmc98@gmail.com>
2021-09-11 18:33:01 +0300

    Rename 'IExClosureAttr' to 'IExClosure', Move 'IExClosure' into 'Interfaces' folder

--- exmat/Interfaces/IExClosure.cs
+++ exmat/Interfaces/IExClosure.cs
@@ -1,13 +1,13 @@
 ﻿namespace ExMat.Interfaces
 {
-    public interface IExClosureAttr
+    public interface IExClosure
     {
         /// <summary>
         /// Return information about the function meta
         /// </summary>
         /// <param name="attr">Meta attribute name</param>
         /// <returns>Integer(param count, def param count, min required args)
-        /// <para>String(function name)</para>
+        /// <para>String(function name, description, return info)</para>
         /// <para>Bool(has vargs, is delegate)</para>
         /// <para>Dictionary<string, ExObject>(default param values)</para>
         /// <para>variable(if delegate found)</para>
